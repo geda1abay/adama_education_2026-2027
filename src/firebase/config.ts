b8ff2f447579d151c8736e7d77314c29a0c1fcb0
Geda Abay <gedaabay@gmail.com>
2026-02-28 09:23:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4711983320-a6099",
+  "appId": "1:48230024947:web:e0dba78e826bbc2d0dd986",
+  "apiKey": "AIzaSyAsGvnO3B1j8xqqdz8_-aCtoE2fH-11voU",
+  "authDomain": "studio-4711983320-a6099.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "48230024947"
+};
