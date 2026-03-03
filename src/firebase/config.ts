1f00a04c9cc012b767dedd913131c19e395d063e
Geda Abay <gedaabay@gmail.com>
2026-03-03 08:12:39 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-4711983320-a6099",
-  "appId": "1:48230024947:web:e0dba78e826bbc2d0dd986",
-  "apiKey": "AIzaSyAsGvnO3B1j8xqqdz8_-aCtoE2fH-11voU",
-  "authDomain": "studio-4711983320-a6099.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "48230024947"
+  projectId: "studio-4711983320-a6099",
+  appId: "1:48230024947:web:e0dba78e826bbc2d0dd986",
+  apiKey: "AIzaSyDVpMXQerxIdeuavnroIHGAKZUgNhd-2GY",
+  authDomain: "studio-4711983320-a6099.firebaseapp.com",
+  messagingSenderId: "48230024947"
 };
